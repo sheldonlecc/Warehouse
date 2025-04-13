e0310697e68869bc72f56b604126ec46c8b18825
“sheldonlecc” <sheldonlee@emails.bjut.edu.cn>
2025-04-14 00:26:02 +0800

    MVC 尝试2

---
+++ src/Model/Weapon.java
@@ -0,0 +1,9 @@
+// weapon class extends item class
+public abstract class Weapon extends Item {
+    public Weapon(String name, double weight) {
+        super(name, weight);
+    }
+
+    // abstract method
+    public abstract void use();
+}    
